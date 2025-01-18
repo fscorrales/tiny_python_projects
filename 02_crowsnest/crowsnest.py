ebaef73458d951ed9253e8f511a8b5fe21f73bd6
Fernando Corrales <corrales_fernando@hotmail.com>
2025-01-18 11:57:51 -0300

    02_crowsnest completed

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Author : Me <me@foo.com>
+Author : Fernando Corrales <fscpython@gmail.com>
 Date   : today
-Purpose: Rock the Casbah
+Purpose: Crow's Nest
 """
 
 import argparse
@@ -13,38 +13,11 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Crow's Nest",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument("word", metavar="word", help="A word")
 
     return parser.parse_args()
 
@@ -54,19 +27,13 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    word = args.word
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    article = "an" if word[0].lower() in "aeiou" else "a"
+
+    print(f"Ahoy, Captain, {article} {word} off the larboard bow!")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
