cc67a295974b056c860a81f3ac14a7755213f6aa
Fernando Corrales <corrales_fernando@hotmail.com>
2025-01-18 12:55:42 -0300

    Update 03_picnic.py's exercise

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Author : Fernando Corrales <fscpython@gmail.com>
+Date   : today
+Purpose: Picnic game
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Picnic game",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("item", metavar="str", help="Item(s) to bring", nargs="+")
+
+    parser.add_argument("-s", "--sorted", help="Sort the items", action="store_true")
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    items = args.item
+    if args.sorted:
+        items.sort()
+
+    if len(items) == 1:
+        bringing = items[0]
+    elif len(items) == 2:
+        bringing = f"{items[0]} and {items[1]}"
+    else:
+        # Oxsford University: "Oxford Comma"
+        items[-1] = f"and {items[-1]}"
+        bringing = ", ".join(items)
+
+    print(f"You are bringing {bringing}.")
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
