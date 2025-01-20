c88269b26fa225a0eea70a424db6a63741e0dae1
Fernando Corrales <corrales_fernando@hotmail.com>
2025-01-20 17:32:16 -0300

    Add completed 06 exercise

--- 06_wc/wc.py
+++ 06_wc/wc.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 """
-Author : Me <me@foo.com>
-Date   : today
-Purpose: Rock the Casbah
+Author : Fernando Corrales <fscorrales@gmail.com>
+Date   : 20-Jan-2025
+Purpose:
+    Learn how to process zero or more positional arguments
+    Validate input files
+    Read from files or from standard input
+    Use multiple levels of for loops
+    Break files into lines, words, and bytes
+    Use counter variables
+    Format string output
 """
 
 import argparse
+import sys
 
 
 # --------------------------------------------------
@@ -13,38 +21,22 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Emulate wc (word count)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument(
+        "file",
+        metavar="FILE",
+        help="Input file(s)",
+        type=argparse.FileType(
+            "rt"
+        ),  # If arguments are provided, they must be readable text files. The files will be opened by argparse and will be provided as file handles
+        nargs="*",  # ?Zero or one | *Zero or more | +One or more
+        default=[
+            sys.stdin
+        ],  # The default will be a list containing sys.stdin, which is like an open file handle to STDIN. We do not need to open it.
+    )
 
     return parser.parse_args()
 
@@ -54,19 +46,23 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    total_lines, total_bytes, total_words = 0, 0, 0
+    for fh in args.file:  # ONE LOOP!
+        num_lines, num_words, num_bytes = 0, 0, 0
+        for line in fh:  # TWO LOOPS!
+            num_lines += 1
+            num_words += len(line.split())
+            num_bytes += len(line)
+        total_lines += num_lines
+        total_bytes += num_bytes
+        total_words += num_words
+        print(f"{num_lines:8}{num_words:8}{num_bytes:8} {fh.name}")
+
+    if len(args.file) > 1:
+        print(f"{total_lines:8}{total_words:8}{total_bytes:8} total")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
