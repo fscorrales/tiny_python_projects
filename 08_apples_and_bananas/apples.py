fda5d7ea6a1cf344b8c7d488cf283921d9420379
Fernando Corrales <corrales_fernando@hotmail.com>
2025-01-26 15:20:55 -0300

    add apples.py 08 exercise

---
+++ 08_apples_and_bananas/apples.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Author : Fernando Corrales <fscpython@gmail.com>
+Date   : 26-Jan-2025
+Purpose:
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Apples and bananas",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("positional", metavar="str", help="A positional argument")
+
+    parser.add_argument(
+        "-v",
+        "--vowel",
+        help="A named string argument",
+        metavar="str",
+        type=str,
+        default="",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="A readable file",
+        metavar="FILE",
+        type=argparse.FileType("r"),
+        default=None,
+    )
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    str_arg = args.arg
+    int_arg = args.int
+    file_arg = args.file
+    flag_arg = args.on
+    pos_arg = args.positional
+
+    print(f'str_arg = "{str_arg}"')
+    print(f'int_arg = "{int_arg}"')
+    print('file_arg = "{}"'.format(file_arg.name if file_arg else ""))
+    print(f'flag_arg = "{flag_arg}"')
+    print(f'positional = "{pos_arg}"')
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
